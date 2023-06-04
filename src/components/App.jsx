45719fd63e8e6abc000ba7ee80092ef0ea728511
Drake Nguyen <drakeknguyen@yahoo.com>
2023-06-04 15:35:56 -0700

    passed setitemDetails and data as props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable */
 import { useState } from 'react';
 
 import { searchArtworks } from '../api';
 import { SearchForm } from './SearchForm';
+import { ImageDetailsPage } from './ImageDetailsPage';
 import { Footer } from './Footer';
 
 import './App.css';
 
 export function App() {
 	const [data, setData] = useState('');
+	const [itemDetails, setitemDetails] = useState('');
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -16,7 +19,6 @@ export function App() {
 		// our UI, we need to make real requests!
 		// @see: ./src/api.js
 		searchArtworks(query).then((json) => {
-			// console.log(json);
 			setData(json.data);
 		});
 	}
@@ -24,19 +26,34 @@ export function App() {
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
-			<SearchForm onSearchSubmit={onSearchSubmit} />
-			{!data ? (
-				<ul></ul>
+			{!itemDetails ? (
+				<>
+					<SearchForm onSearchSubmit={onSearchSubmit} />
+					{!data ? (
+						<ul></ul>
+					) : (
+						<ul>
+							{data.map((el) => {
+								return (
+									<li
+										className="listItem"
+										onMouseDown={() => {
+											setitemDetails(el);
+										}}
+										key={el.image_id}
+									>
+										{el.title} - {el.artist_title}
+									</li>
+								);
+							})}
+						</ul>
+					)}
+				</>
 			) : (
-				<ul>
-					{data.map((el) => {
-						return (
-							<li key={el.image_id} className="listItem">
-								{el.title} - {el.artist_title}
-							</li>
-						);
-					})}
-				</ul>
+				<ImageDetailsPage
+					data={itemDetails}
+					resetitemDetails={setitemDetails}
+				/>
 			)}
 			<Footer />
 		</div>
