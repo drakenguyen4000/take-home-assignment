3aa1ed28d236d5bf33171447a81c5c33492e0190
Drake Nguyen <drakeknguyen@yahoo.com>
2023-06-04 15:36:52 -0700

    display image details

--- src/components/ImageDetailsPage.jsx
+++ src/components/ImageDetailsPage.jsx
@@ -1,3 +1,17 @@
-export function ImageDetailsPage() {
-	return <div></div>;
+export function ImageDetailsPage({ data, resetitemDetails }) {
+	function onBack() {
+		resetitemDetails('');
+	}
+	return (
+		<div>
+			<button onClick={onBack}>Back</button>
+			<p>{data.title}</p>
+			<p>{data.artist_title}</p>
+			<img
+				alt={data.thumbnail.alt_text}
+				id="portrait"
+				src={`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`}
+			/>
+		</div>
+	);
 }
